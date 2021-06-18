2607c7da10ddd06db4ad315cf5b4ceeb55aa6df1
Vlad Tupchiienko <vt.progr@gmail.com>
2021-06-18 21:04:23 +0300

    Initial commit

---
+++ src/main/java/com/cursor/movierewiev/MovieRewievApplication.java
@@ -0,0 +1,13 @@
+package com.cursor.movierewiev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRewievApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRewievApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursor/movierewiev/MovieRewievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursor.movierewiev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRewievApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
